18825b4893b89fc0f702ea1b15ee6e0fef8e8243
Sébastien Geiser <sgeiser@precitrame.com>
2019-03-05 07:32:52 +0100

    Retrait de methodDecorator. x64 still working

--- CSharpRegexTools4Npp/Properties/AssemblyInfo.cs
+++ CSharpRegexTools4Npp/Properties/AssemblyInfo.cs
@@ -1,6 +1,4 @@
-﻿using CSharpRegexTools4Npp.Utils;
-using System.Reflection;
-using System.Runtime.CompilerServices;
+﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
@@ -35,5 +33,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.1.0")]
 [assembly: AssemblyFileVersion("1.0.1.0")]
-[module: Interceptor]
 
