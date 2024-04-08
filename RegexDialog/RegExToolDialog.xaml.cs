ee33e31c60b055143bad8bcc7824d254c0f281ea
Sébastien Geiser <sgeiser@precitrame.com>
2024-04-08 15:13:09 +0200

    refactoring

--- RegexDialog/RegExToolDialog.xaml.cs
+++ RegexDialog/RegExToolDialog.xaml.cs
@@ -218,7 +218,7 @@ namespace RegexDialog
 
             InitializeComponent();
 
-            Title += " - " + Assembly.GetCallingAssembly().GetName().Version.ToString();
+            Title += $" - {Assembly.GetCallingAssembly().GetName().Version}";
 
             Init();
         }
