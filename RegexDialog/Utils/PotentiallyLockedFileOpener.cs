156cda60315483f36c36d18b7af5830250e87b22
Sébastien Geiser <SGeiser@precitrame.com>
2025-05-06 08:52:11 +0200

    Handles locked Excel files by creating copies
    Addresses the issue where the application fails to open Excel files if they are locked by another process.
    
    Implements a mechanism to create a temporary copy of the Excel file if the original is locked, allowing the application to proceed without interruption. This ensures continued functionality even when the Excel file is in use elsewhere.

---
+++ RegexDialog/Utils/PotentiallyLockedFileOpener.cs
@@ -0,0 +1,44 @@
+﻿using System.IO;
+
+namespace RegexDialog.Utils
+{
+    public static class PotentiallyLockedFileCopier
+    {
+        public static string MakeCopyIfLocked(this string sourceFile)
+        {
+            if(IsFileLocked(new FileInfo(sourceFile)))
+            {
+                string destinationFile = Path.Combine(Path.GetTempPath(), Path.GetFileName(sourceFile));
+
+                if (!File.Exists(destinationFile) || File.GetLastWriteTime(sourceFile) != File.GetLastWriteTime(destinationFile))
+                {
+                    File.Copy(sourceFile, destinationFile);                    
+                }
+
+                return destinationFile;
+
+            }
+            else
+            {
+                return sourceFile;
+            }
+        }
+
+        private static bool IsFileLocked(FileInfo file)
+        {
+            try
+            {
+                using (FileStream stream = file.Open(FileMode.Open, FileAccess.Read, FileShare.None))
+                {
+                    stream.Close();
+                }
+            }
+            catch (IOException)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
