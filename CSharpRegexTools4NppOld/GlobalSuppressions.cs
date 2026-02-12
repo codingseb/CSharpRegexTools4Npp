cf5280eca76e929e02beb368efe2caf29c0bf489
CondingSeb <sebgeiser@gmail.com>
2026-02-12 10:31:38 +0100

    chore: Removes old project files
    Removes the old project files as they are no longer needed.
    The project has been migrated to a new structure.

--- CSharpRegexTools4NppOld/GlobalSuppressions.cs
+++
@@ -1,9 +0,0 @@
-﻿// This file is used by Code Analysis to maintain SuppressMessage
-// attributes that are applied to this project.
-// Project-level suppressions either have no target or are given
-// a specific target and scoped to a namespace, type, member, etc.
-
-using System.Diagnostics.CodeAnalysis;
-
-[assembly: SuppressMessage("Redundancy", "RCS1163:Unused parameter.", Justification = "<Pending>", Scope = "namespaceanddescendants", Target = "~N:CSharpRegexTools4Npp")]
-[assembly: SuppressMessage("Style", "IDE0060:Remove unused parameter", Justification = "<Pending>", Scope = "namespaceanddescendants", Target = "~N:CSharpRegexTools4Npp")]
